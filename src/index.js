5c87185780cc2bcfdf58f1c83eb949bf92738c7f
Stefano Germani <stefger88@gmail.com>
2018-06-12 02:20:56 +0200

    added form validation

--- src/index.js
+++ src/index.js
@@ -1,84 +1,81 @@
 import Scommessa from './scommessa';
 
 
-$(document).ready(function () {
+$('.quota').on('input', function () {
+    $('#txtPuntata1').val('');
+    $('#txtPuntataX').val('');
+    $('#txtPuntata2').val('');
 
-    $('.quota').on('input', function () {
-        $('#txtPuntata1').val('');
-        $('#txtPuntataX').val('');
-        $('#txtPuntata2').val('');
+    $('#txtProbabilita1').val('');
+    $('#txtProbabilitaX').val('');
+    $('#txtProbabilita2').val('');
 
-        $('#txtProbabilita1').val('');
-        $('#txtProbabilitaX').val('');
-        $('#txtProbabilita2').val('');
+    $('#btnCalcola').removeClass('btn-success');
+    $('#btnCalcola').removeClass('btn-danger');
+    $('#btnCalcola').addClass('btn-primary');
 
-        $('#btnCalcola').removeClass('btn-success');
-        $('#btnCalcola').removeClass('btn-danger');
-        $('#btnCalcola').addClass('btn-primary');
+    $('#result').text('');
+});
 
-        $('#result').text('');
+$('.puntata').on('input', function () {
+    $('.puntata').not($(this)).val('');
+
+    $('#btnCalcola').removeClass('btn-success');
+    $('#btnCalcola').removeClass('btn-danger');
+    $('#btnCalcola').addClass('btn-primary');
+
+    $('#result').text('');
+});
+
+$('#bettyForm').submit(function (event) {
+    event.preventDefault();
+
+    var quota1 = parseFloat($('#txt1').val());
+    var quota2 = parseFloat($('#txtX').val());
+    var quota3 = parseFloat($('#txt2').val());
+
+    var puntata1 = parseFloat($('#txtPuntata1').val());
+    var puntata2 = parseFloat($('#txtPuntataX').val());
+    var puntata3 = parseFloat($('#txtPuntata2').val());
+
+    if (isNaN(quota3)) {
+        quota3 = 0;
+    }
+
+    var scommessa = new Scommessa({
+        quota: quota1,
+        puntata: puntata1,
+    }, {
+        quota: quota2,
+        puntata: puntata2,
+
+    }, {
+        quota: quota3,
+        puntata: puntata3,
     });
 
-    $('.puntata').on('input', function () {
-        $('.puntata').not($(this)).val('');
+    const probabilità = scommessa.calcolaProbabilta();
 
-        $('#btnCalcola').removeClass('btn-success');
-        $('#btnCalcola').removeClass('btn-danger');
-        $('#btnCalcola').addClass('btn-primary');
+    $('#txtProbabilita1').val(`${probabilità[0]} %`);
+    $('#txtProbabilitaX').val(`${probabilità[1]} %`);
+    $('#txtProbabilita2').val(`${probabilità[2]} %`);
 
-        $('#result').text('');
-    });
+    const puntate = scommessa.calcolaPuntate();
 
-    $('#btnCalcola').click(function (event) {
-        event.preventDefault();
+    $('#txtPuntata1').val(puntate[0]);
+    $('#txtPuntataX').val(puntate[1]);
+    $('#txtPuntata2').val(puntate[2]);
 
-        var quota1 = parseFloat($('#txt1').val());
-        var quota2 = parseFloat($('#txtX').val());
-        var quota3 = parseFloat($('#txt2').val());
+    if (scommessa.isGuagagno()) {
+        $('#btnCalcola').removeClass('btn-primary');
+        $('#btnCalcola').addClass('btn-success');
+    } else {
+        $('#btnCalcola').removeClass('btn-primary');
+        $('#btnCalcola').addClass('btn-danger');
+    }
 
-        var puntata1 = parseFloat($('#txtPuntata1').val());
-        var puntata2 = parseFloat($('#txtPuntataX').val());
-        var puntata3 = parseFloat($('#txtPuntata2').val());
-
-        if (isNaN(quota3)) {
-            quota3 = 0;
-        }
-
-        var scommessa = new Scommessa({
-            quota: quota1,
-            puntata: puntata1,
-        }, {
-            quota: quota2,
-            puntata: puntata2,
-
-        }, {
-            quota: quota3,
-            puntata: puntata3,
-        });
-
-        const probabilità = scommessa.calcolaProbabilta();
-
-        $('#txtProbabilita1').val(`${probabilità[0]} %`);
-        $('#txtProbabilitaX').val(`${probabilità[1]} %`);
-        $('#txtProbabilita2').val(`${probabilità[2]} %`);
-
-        const puntate = scommessa.calcolaPuntate();
-
-        $('#txtPuntata1').val(puntate[0]);
-        $('#txtPuntataX').val(puntate[1]);
-        $('#txtPuntata2').val(puntate[2]);
-
-        if (scommessa.isGuagagno()) {
-            $('#btnCalcola').removeClass('btn-primary');
-            $('#btnCalcola').addClass('btn-success');
-        } else {
-            $('#btnCalcola').removeClass('btn-primary');
-            $('#btnCalcola').addClass('btn-danger');
-        }
-
-        var percGuadagno = scommessa.calcolaPercentualeGuadagno();
-        $('#result').text(
-            `Somma puntate: ${scommessa.sommaPuntate} €; Percentuale guadagno: ${percGuadagno}%`
-        );
-    });
+    var percGuadagno = scommessa.calcolaPercentualeGuadagno();
+    $('#result').text(
+        `Somma puntate: ${scommessa.sommaPuntate} €; Percentuale guadagno: ${percGuadagno}%`
+    );
 });
